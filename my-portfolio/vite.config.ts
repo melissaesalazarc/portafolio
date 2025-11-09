5b26c3034dd1e052ecc195722e4f61fd8f262c4a
Melissa Salazar <95241712+melissaesalazarc@users.noreply.github.com>
2025-11-09 14:56:58 -0600

    .

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // así sirve para rutas relativas y evita problemas de subcarpeta
+  base: "/", // 🔹 usa "/" para Vercel
 });
