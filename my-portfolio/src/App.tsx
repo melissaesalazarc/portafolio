a49d48e977411b60625fba85e4c6a98b66b2c064
Melissa Salazar <95241712+melissaesalazarc@users.noreply.github.com>
2025-11-09 14:15:00 -0600

    .

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Section from "./components/Section";
+//import Section from "./components/Section";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Projects from "./components/Projects";
