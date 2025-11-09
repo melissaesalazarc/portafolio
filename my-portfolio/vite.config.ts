d54d1deffbb9c6bc7c20cb12187e5c40be0d01eb
Melissa Salazar <95241712+melissaesalazarc@users.noreply.github.com>
2025-11-09 15:03:34 -0600

    index

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 🔹 usa "/" para Vercel
+  base: "/", // <-- aquí es importante que sea "/" para Vercel
 });
